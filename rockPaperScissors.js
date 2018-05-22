fd2fe10eabb3275ba16811454d318c21505ce466
Christian Clark <christianclark687@gmail.com>
2018-05-22 00:12:16 -0500

    Cleaned up code, began adding button functionality
    Removed old code for playing in the browser console, added eventListener to the buttons for rock/paper/scissors.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,4 +1,12 @@
 var score = {player: 0, comp: 0};
+var submit = document.querySelectorAll('button');
+console.log(submit);
+
+submit.forEach(function(btn){
+  btn.addEventListener('click', function(event) {
+    var rps = event.target.id;
+  })
+})
 
 function computerPlay() {
   var ran = Math.floor(Math.random() * 3);
@@ -15,61 +23,6 @@ function computerPlay() {
   }
 }
 
-function playRound(playerChoice, computerChoice) {
-  if (playerChoice.toLowerCase() == "rock") {
-    switch (computerChoice) {
-      case "rock":
-        return "Rock and rock... It's a tie!";
-        break;
-      case "paper":
-        computerWins++;
-        return "Paper covers rock. You lose.";
-        break;
-      default:
-        playerWins++;
-        return "Rock smashes scissors. You win!";
-      }
-  } else if (playerChoice.toLowerCase() == "paper") {
-    switch (computerChoice) {
-      case "rock":
-        playerWins++;
-        return "Paper covers rock. You win!";
-        break;
-      case "paper":
-        return "Paper and paper... It's a tie!";
-        break;
-      default:
-        computerWins++;
-        return "Scissors cut paper. You lose.";
-      }
-  } else if (playerChoice.toLowerCase() == "scissors") {
-    switch (computerChoice) {
-      case "rock":
-        computerWins++;
-        return "Rock smashes scissors. You lose.";
-        break;
-      case "paper":
-        playerWins++;
-        return "Scissors cut paper. You win!";
-        break;
-      default:
-        return "Scissors and scissors... It's a tie.";}
-  } else {
-    playerChoice = prompt("Let's try that again. Rock, paper, or scissors?")
-  }
-}
-
-for (playerWins = 0; playerWins < 5 && computerWins <5;) {
-  playerChoice = prompt("Rock, paper, or scissors?");
-  computerChoice = computerPlay();
-  console.log(playRound(playerChoice, computerChoice));
-  console.log("The score is player: " + playerWins + " to computer: " + computerWins + ".");
-}
-
-if (playerWins = 4) {
-  console.log("You won five games in a row! Congrats!");
-}
-
-if (computerWins = 4) {
-  console.log("Aww, you lost five games in a row. Bummer.");
+function playerChoice(choice) {
+  console.log(choice);
 }

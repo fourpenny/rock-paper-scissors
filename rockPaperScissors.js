5e885fbc52d1e90eb7f6f23676792d1284200d84
Christian Clark <christianclark687@gmail.com>
2018-05-22 14:03:46 -0500

    Made game functional
    Created functions to play the game when the buttons are clicked by the player. The text elements with the score update after each round and the results are also logged in the console.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,12 +1,19 @@
 var score = {player: 0, comp: 0};
 var submit = document.querySelectorAll('button');
-console.log(submit);
+var playerScore = document.getElementById('player-score');
+var computerScore = document.getElementById('computer-score');
+var result;
 
 submit.forEach(function(btn){
-  btn.addEventListener('click', function(event) {
-    var rps = event.target.id;
-  })
-})
+  btn.addEventListener('click', playRound);
+});
+
+function playRound(event) {
+  var playerChoice = event.target.id;
+  compareResult(playerChoice, computerPlay());
+  console.log(result);
+  console.log(score);
+}
 
 function computerPlay() {
   var ran = Math.floor(Math.random() * 3);
@@ -23,6 +30,52 @@ function computerPlay() {
   }
 }
 
-function playerChoice(choice) {
-  console.log(choice);
+function compareResult (player, computer){
+  if (player == "r") {
+    switch (computer) {
+      case "rock":
+        result = "tie";
+        break;
+      case "paper":
+        result = "lose";
+        score.comp++;
+        postResult;
+        break;
+      default:
+        result = "win";
+        score.player++;
+    }
+  } else if (player == "p") {
+    switch (computer) {
+      case "rock":
+        result = "win";
+        score.player++;
+        break;
+      case "paper":
+        result = "tie";
+        break;
+      default:
+        result = "lose";
+        score.comp++;
+    }
+  } else {
+    switch (computer) {
+      case "rock":
+        result = "lose";
+        score.comp++;
+        break;
+      case "paper":
+        result = "win";
+        score.player++;
+        break;
+      default:
+        result = "tie";
+    }
+  }
+  postResult();
+}
+
+function postResult() {
+  playerScore.textContent = score.player;
+  computerScore.textContent = score.comp;
 }

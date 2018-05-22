0c467ffda860ec5192f8966b993a738721456d1b
Christian Clark <christianclark687@gmail.com>
2018-05-22 14:52:20 -0500

    Results posted after each round
    The newly added postResult function adds text information about each round's result after the round into an unordered list at the bottom of the page.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,8 +1,13 @@
+//Variables used for storing and displaying the score
 var score = {player: 0, comp: 0};
 var submit = document.querySelectorAll('button');
 var playerScore = document.getElementById('player-score');
 var computerScore = document.getElementById('computer-score');
+
+//Variables for logging the results
 var result;
+var resultText;
+var resultsList = document.querySelector('ul');
 
 submit.forEach(function(btn){
   btn.addEventListener('click', playRound);
@@ -11,7 +16,7 @@ submit.forEach(function(btn){
 function playRound(event) {
   var playerChoice = event.target.id;
   compareResult(playerChoice, computerPlay());
-  console.log(result);
+  postResult(resultText);
   console.log(score);
 }
 
@@ -32,50 +37,65 @@ function computerPlay() {
 
 function compareResult (player, computer){
   if (player == "r") {
+    //Switch for rock
     switch (computer) {
       case "rock":
         result = "tie";
+        resultText = "Rock and rock... It's a tie!"
         break;
       case "paper":
         result = "lose";
         score.comp++;
-        postResult;
+        resultText = "Paper covers rock. You lose."
         break;
       default:
         result = "win";
         score.player++;
+        resultText = "Rock smashes scissors. You win!"
     }
   } else if (player == "p") {
+    //Switch for paper
     switch (computer) {
       case "rock":
         result = "win";
         score.player++;
+        resultText = "Rock smashes scissors. You lose."
         break;
       case "paper":
         result = "tie";
+        resultText = "Scissors and scissors... It's a tie."
         break;
       default:
         result = "lose";
         score.comp++;
+        resultText = "Scissors cut paper. You lose."
     }
   } else {
+    //Switch for scissors
     switch (computer) {
       case "rock":
         result = "lose";
         score.comp++;
+        resultText = "Rock smashes scissors. You lose.";
         break;
       case "paper":
         result = "win";
         score.player++;
+        resultText = "Scissors cut paper. You win!"
         break;
       default:
         result = "tie";
+        resultText = "Scissors and scissors... It's a tie.";
     }
   }
-  postResult();
 }
 
-function postResult() {
+function postResult(resultText) {
   playerScore.textContent = score.player;
   computerScore.textContent = score.comp;
+
+  var li = document.createElement('li');
+  li.className = result + ' list';
+  li.appendChild(document.createTextNode(resultText));
+  resultsList.appendChild(li);
 }
